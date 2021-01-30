c1de564f238129d112ac99655b6dc92980ce612e
PavelGoreviy <paul.robin@live.com>
2021-01-30 21:07:58 +0300

    Some fixed

--- js/main.js
+++ js/main.js
@@ -1,46 +1,47 @@
-const getRandomNumber = function (num1, num2) {
+const getRandomInteger = function (num1, num2) {
 
-  if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    // Выход из функции при невалидном значении
     return
   }
 
-  let minNumber = Math.min(num1, num2);
-  let maxNumber = Math.max(num1, num2);
+  const minNumber = Math.ceil(Math.min(num1, num2));
+  const maxNumber = Math.floor(Math.max(num1, num2));
 
-  minNumber = Math.ceil(minNumber);
-  maxNumber = Math.floor(maxNumber);
-
-  minNumber = minNumber > 0 ? minNumber : 0;
+  if (minNumber < 0 && maxNumber <0) {
+    // Выход из функции при значении меньше 0
+    return
+  }
 
   if (maxNumber === minNumber) {
     return minNumber;
   }
 
   if (minNumber >= 0 && maxNumber >= 0 && maxNumber > minNumber) {
-    return  Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+    return  Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
   }
 }
 
-getRandomNumber(1, 4);
-
 const getRandomFloatNumber = function (num1, num2, range) {
 
-  if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    // Выход из функции при невалидном значении
     return
   }
 
-  let minNumber = Math.min(num1, num2);
-  let maxNumber = Math.max(num1, num2);
+  const minNumber = Math.min(num1, num2);
+  const maxNumber = Math.max(num1, num2);
 
-  minNumber = minNumber > 0 ? minNumber : 0;
+  if (minNumber < 0 && maxNumber <0) {
+    // Выход из функции при значении меньше 0
+    return
+  }
 
   if (maxNumber === minNumber) {
     return minNumber;
   }
 
-  if (num1 >= 0 && num2 >= 0 && num2 > num1) {
-    return ((Math.random() * (num2 - num1)) + num1).toFixed(range);
+  if (minNumber >= 0 && maxNumber >= 0 && maxNumber > minNumber) {
+    return ((Math.random() * (maxNumber - minNumber)) + minNumber).toFixed(range);
   }
 }
-
-getRandomFloatNumber(1.2222, 5.1111111, 4);
